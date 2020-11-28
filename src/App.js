114ca156f058344a797ecde84342e181bb147d3c
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-27 21:14:40 -0800

    feat: add Resource users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import './App.css';
 
@@ -7,7 +7,9 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
-    <Admin dataProvider={dataProvider} />
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser}></Resource>
+    </Admin>
   );
 }
 
