6ae1b86ca8e55f2e4d6b40ba0d9c77174e359fd8
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-28 02:17:44 -0800

    feat: add users.js from generated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource/*, ListGuesser*/ } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
 import './App.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
@@ -8,9 +9,9 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 (() => {
     const oldLogError = console.error;
     console.error = function(...args) {
-        if (typeof args[0] !== 'string' || args[0].indexOf('is deprecated in StrictMode') === -1 ||
+        if (typeof args[3] !== 'string' || args[3] !== 'Transition' ||
             typeof args[1] !== 'string' || args[1] !== 'findDOMNode' ||
-            typeof args[3] !== 'string' || args[3] !== 'transition'
+            typeof args[0] !== 'string' || args[0].indexOf('is deprecated in StrictMode') === -1
             )
         { // arg[1,2] === 'findDOMNode'; arg[3] === 'transition'
             oldLogError.apply(console, args);
@@ -21,7 +22,8 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser}></Resource>
+        {/* <Resource name="users" list={ListGuesser}></Resource> */}
+        <Resource name="users" list={UserList}></Resource>
     </Admin>
   );
 }
