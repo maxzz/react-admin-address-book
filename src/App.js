140a56978e1febf9c5a241f9ad92de5b56ca2e3b
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-28 02:04:24 -0800

    fix: React warning findDOMNode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import './App.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
+(() => {
+    const oldLogError = console.error;
+    console.error = function(...args) {
+        if (typeof args[0] !== 'string' || args[0].indexOf('is deprecated in StrictMode') === -1 ||
+            typeof args[1] !== 'string' || args[1] !== 'findDOMNode' ||
+            typeof args[3] !== 'string' || args[3] !== 'transition'
+            )
+        { // arg[1,2] === 'findDOMNode'; arg[3] === 'transition'
+            oldLogError.apply(console, args);
+        } else {console.log('me', args);}
+    }
+})();
+
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
