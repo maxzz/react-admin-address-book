760066e89cdc5bd236a21c5e55b0b62a3309e0a4
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-28 18:12:08 -0800

    feat: add posts list viewer component trace

--- src/posts.js
+++ src/posts.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { List, Datagrid, ReferenceField, TextField } from 'react-admin';
 
-export const PostList = props => (
-    <List {...props}>
+export const PostList = props => {
+    console.log(props);
+    return <List {...props}>
         <Datagrid rowClick="edit">
-            {console.log(props)}
             <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
         </Datagrid>
     </List>
-);
+};
