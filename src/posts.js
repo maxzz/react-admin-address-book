d7137aa2116729249b1639b32a779357aa47c5b1
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-28 18:14:29 -0800

    feat: post shows real user name vs. ID

--- src/posts.js
+++ src/posts.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { List, Datagrid, ReferenceField, TextField } from 'react-admin';
 
 export const PostList = props => {
-    console.log(props);
     return <List {...props}>
         <Datagrid rowClick="edit">
-            <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
