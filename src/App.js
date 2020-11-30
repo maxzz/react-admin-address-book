0c643f18c6adbf5d9e10340805a559ed9de1cec4
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-29 17:44:38 -0800

    feat: block router warnings in non-production environment

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import './App.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-(() => {
-    const oldLogError = console.error;
-    console.error = function(...args) {
-        if (typeof args[3] !== 'string' || args[3] !== 'Transition' ||
-            typeof args[1] !== 'string' || args[1] !== 'findDOMNode' ||
-            typeof args[0] !== 'string' || args[0].indexOf('is deprecated in StrictMode') === -1
-            )
-        { // arg[1,2] === 'findDOMNode'; arg[3] === 'transition'
-            oldLogError.apply(console, args);
-        } //else {console.log('me', args);}
-    }
-})();
+if (process.env.NODE_ENV !== 'production') {
+    (() => {
+        const oldLogError = console.error;
+        console.error = function(...args) {
+            if (typeof args[3] !== 'string' || args[3] !== 'Transition' ||
+                typeof args[1] !== 'string' || args[1] !== 'findDOMNode' ||
+                typeof args[0] !== 'string' || args[0].indexOf('is deprecated in StrictMode') === -1
+                )
+            { // arg[1,2] === 'findDOMNode'; arg[3] === 'transition'
+                oldLogError.apply(console, args);
+            } //else {console.log('me', args);}
+        }
+        const oldLogWarn = console.warn;
+        console.warn = function(...args) {
+            if (typeof args[0] !== 'string' || args[0].indexOf('history') === -1) {
+                oldLogWarn.apply(console, args);
+            }
+        }
+    })();
+}
 
 function App() {
   return (
