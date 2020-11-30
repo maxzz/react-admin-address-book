d686079fdb4ca9c71a95606c65adf0979744146a
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-29 19:43:23 -0800

    feat: add icons to main menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Admin, Resource/*, ListGuesser*/ /*, EditGuesser */ } from 'react-admin
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 import { PostList, PostCreate, PostEdit } from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 import './App.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
@@ -31,10 +33,10 @@ if (process.env.NODE_ENV !== 'production') {
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} />
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon} />
         {/* <Resource name="posts" list={PostList} edit={EditGuesser} /> */}
         {/* <Resource name="users" list={ListGuesser}></Resource> */}
-        <Resource name="users" list={UserList}></Resource>
+        <Resource name="users" list={UserList} icon={UserIcon}></Resource>
     </Admin>
   );
 }
