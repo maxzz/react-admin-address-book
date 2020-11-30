86676b3e0480803b4985803ed316b0022cde7bbb
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-29 18:01:21 -0800

    feat: add more prominent edit button per each row

--- src/posts.js
+++ src/posts.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
-import { List, Datagrid, ReferenceField, TextField } from 'react-admin';
+import { List, Datagrid, ReferenceField, TextField, EditButton } from 'react-admin';
 
 export const PostList = props => {
     return <List {...props}>
-        <Datagrid rowClick="edit">
+        {/* <Datagrid rowClick="edit"> */}
+        <Datagrid>
+            <TextField label="ID" source="id" />
             <ReferenceField label="Alien vs Name" source="userId" reference="users">
                 <TextField source="name" />
             </ReferenceField>
-            <TextField label="ID" source="id" />
+            {/* <TextField label="ID" source="id" /> */}
             <TextField source="title" />
-            <TextField source="body" />
+            {/* <TextField source="body" /> */}
+            <EditButton />
         </Datagrid>
     </List>
 };
